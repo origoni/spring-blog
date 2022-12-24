3e88e3b2fafc7942989d5eff380401ae67064ed8
origoni <dk.lee@kakaocorp.com>
2022-12-24 12:03:06 +0900

    springboot3 테스트 완료

--- src/main/java/com/millky/blog/application/configuration/SecurityConfig.java
+++ src/main/java/com/millky/blog/application/configuration/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.millky.blog.application.configuration;
 
 import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 //import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -11,15 +12,26 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-//@Configuration
+import java.util.List;
+
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig { // } extends WebSecurityConfigurerAdapter {
 
+    List<String> publicApis = List.of("/generate/**", "validated/**");
+
+
+
     //	@Override
     @Bean
+//    public
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
         // @formatter:off
+
+
         return http
+
+
                 .oauth2Login()
                     .loginPage("/user/login")
                 .and()
@@ -27,12 +39,12 @@ public class SecurityConfig { // } extends WebSecurityConfigurerAdapter {
                         .logoutUrl("/user/logout")
                         .logoutSuccessUrl("/")
                 .and()
-                    .authorizeRequests()
+                .authorizeRequests()
+
                 .requestMatchers(new AntPathRequestMatcher("/**/write*")).authenticated()
                 .requestMatchers(new AntPathRequestMatcher("/**/edit*")).authenticated()
                 .requestMatchers(new AntPathRequestMatcher("/**/delete*")).authenticated()
                 .requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
-
 //                .antMatchers("/**/write*", "/**/edit*", "/**/delete*").authenticated()
 //                        .antMatchers("/**").permitAll()
                 .and()
@@ -44,6 +56,8 @@ public class SecurityConfig { // } extends WebSecurityConfigurerAdapter {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {// void configure(WebSecurity web) throws Exception {
         return (web) -> web.ignoring() .requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+//                .requestMatchers(new AntPathRequestMatcher("/**"))
+//                .requestMatchers(new AntPathRequestMatcher("/swagger-ui.html"));
                 //antMatchers("/h2-console/**");
     }
 }
